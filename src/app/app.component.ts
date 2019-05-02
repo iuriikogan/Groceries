fe3040527590205d9a924cdca56d6f467afa9bca
Iurii Kogan <koganiurii@gmail.com>
2019-05-02 22:18:33 +0700

    data-binding login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
 import { Component } from "@angular/core";
-import { calcBindingFlags } from "@angular/core/src/view/util";
+import { User } from "./shared/user/user"
 
 @Component({
     selector: "gr-main",
-    template:
-        `<StackLayout>
-            <Image src="~logo_login" stretch="none" horizontalAlignment="center"></Image>
-            <TextField hint="Email Address" keyboardType="email" autocorrect="false" autocapitalizationType="none"></TextField>
-            <TextField hint="Password" secure="true"></TextField>
-
-            <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
-            <Button text="Sign up for Groceries"></Button>
-        </StackLayout>
-    `,
+    templateUrl: "app.pages/login.component.html",
     styleUrls: ['app/pages/login/login.css','app/pages/login/login-common.css']
 })
 export class AppComponent { 
 
+    public user: User;
+    isLoggingIn = true;
+
+    constructor() {
+        this.user = new User()
+    }
+
     public submit() {
-        console.log("hello");
+        alert('You\'re using ' + this.user.email)
+    }
+
+    public toggleDisplay() {
+        this.isLoggingIn = !this.isLoggingIn;
     }
 } 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+ import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule} from 'nativescript-angular/forms';
 
-import { AppComponent } from "./app.component";
+import {AppComponent} from './app.component';
 
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
-        NativeScriptModule
+        NativeScriptModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         AppComponent
